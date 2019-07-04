efecc7c70c1fe2c8fc6bc14952d34712730d2321
Parth-Dave <parthdave718@gmail.com>
2019-07-05 02:13:31 +0530

    working for powers of 2

--- assign3.c
+++ assign3.c
@@ -25,18 +25,19 @@ int main_i=0;
 void* comp_multiply(void *arg){
 	int i =main_i;
 	main_i++;
+	complex_int *arr_pointer= (complex_int *)arg;
 	//printf("arg recieved is %d\n",i );
 	//int val=*i;
 	//printf("val is %d\n",val);
 	//printf("D %d has real %d and complex %d \n",val,D[val].real,D[val].complex);
-	complex_int a=D[i*2];
+	complex_int a=arr_pointer[i*2];
 	//printf("A has real %d and complex %d \n",a.real,a.complex);
-	complex_int b=D[(i*2)+1];
+	complex_int b=arr_pointer[(i*2)+1];
 	//printf("B has real %d and complex %d \n",b.real,b.complex);
 	complex_int c;
 	c.real=(a.real*b.real) -(a.complex*b.complex);
 	c.complex=(a.complex*b.real)+(a.real*b.complex);
-	//printf("C has real %d and complex %d \n",c.real,c.complex);
+	printf("C has real %d and complex %d \n",c.real,c.complex);
 	temp[i]=c;
 
 }
@@ -62,7 +63,7 @@ complex_int *data_readfile(){
 		scanf("%c",&c);
 		scanf("%d",&j);
 		C_array[i].complex=j;
-	//	printf("i is %d and real is %d and complex is %d \n",i,C_array[i].real,C_array[i].complex);
+		//printf("i is %d and real is %d and complex is %d \n",i,C_array[i].real,C_array[i].complex);
 	}
 	return C_array;
 }
@@ -84,18 +85,24 @@ int main(int argc,char **argv){
 	D = data_readfile();	
 	int t;
 	// program_var main_struct;
-	pthread_t threads[N/2];
-	printf("creating %d threads\n",N/2 );
-	temp=(complex_int *)malloc((N/2)*sizeof(complex_int));
-	for(t=0;t<N/2;t++){
-		//printf("arg sent is %d\n",t );
-		pthread_create(&threads[t],NULL,comp_multiply,(void *)&t);
+	while(N>1){	
+		main_i=0;
+		pthread_t threads[N/2];
+		printf("creating %d threads\n",N/2 );
+		temp=(complex_int *)malloc((N/2)*sizeof(complex_int));
+		for(t=0;t<N/2;t++){
+			//printf("arg sent is %d\n",t );
+			pthread_create(&threads[t],NULL,comp_multiply,(void *)D);
+		}
+		for(t=0;t<(N/2);t++){
+			pthread_join(threads[t], NULL);
+		}
+		for(t=0;t<(N/2);t++){
+			D[t]=temp[t];
+		}
+		N=N/2;
 	}
-	for(t=0;t<(N/2);t++){
-		pthread_join(threads[t], NULL);
-	}
-
-	
+	printf("Final multiplication is %d + i %d \n",D[0].real,D[0].complex );
 	// for(int i=0;i<(N/2);i++){
 	// 	printf("i is %d and real is %d and complex is %d \n",i,temp[i].real,temp[i].complex);
 	// }
